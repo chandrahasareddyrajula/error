819f456b07eb7dda71feb40de941e074b1977ba6
chandrahasa <chandrahasareddy12506@gmail.com>
2017-03-08 14:17:08 +0530

    this is java error

---
+++ IfElseExample.java
@@ -0,0 +1,10 @@
+public class IfElseExample {  
+public static void main(String[] args) {  
+    char =15;  
+    if(number2==0){  
+        System.out.println("even number"); 
+    }else{  
+        System.out.println("odd number");  
+    }  
+}  
+}  
